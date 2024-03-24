0febbf6f58888da4822bd1ac596db9436588054b
mayurjadhav2002 <mayurshrikantjadhav@gmail.com>
2024-03-24 07:15:11 +0530

    mail page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Header from "./Landing/Header";
+import Main from "./Landing/Main";
 
 export default function Home() {
   return (
     <>
     <Header/>
+    <Main/>
     </>
   );
 }
